3ad8974363068bc99720051eece61c5ced931f17
Ofek <itzofek@gmail.com>
2018-01-20 15:25:54 +0200

    Test 1

---
+++ src/org/usfirst/frc/team5747/robot/OI.java
@@ -0,0 +1,27 @@
+package org.usfirst.frc.team5747.robot;
+
+import org.usfirst.frc.team5747.util.XboXUID;
+
+/**
+ * This class is the glue that binds the controls on the physical operator
+ * interface to the commands and command groups that allow control of the robot.
+ */
+public class OI {
+	private XboXUID driver = new XboXUID(0);
+	
+	public OI() {
+	}
+	
+
+
+
+	public double getForwardDriver() {
+		return  Math.pow (driver.getLeftY(), 3);
+	}
+
+	public double getRotationDriver() {
+		return Math.pow(driver.getRightX(), 3);
+	}
+
+
+}
